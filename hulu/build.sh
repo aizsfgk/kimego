c60d16b8f40669823f5fe272cc4686a2192dedfe
aizsfgk <1732756017@qq.com>
2021-06-16 19:30:22 +0800

    add some note

--- hulu/build.sh
+++ hulu/build.sh
@@ -4,4 +4,8 @@
 HULU_VERSION="0.0.1"
 GIT_COMMIT=$(git rev-parse HEAD)
 
-go build -ldflags "-X main.version=$HULU_VERSION -X main.commit=$GIT_COMMIT"
+go build -ldflags "-X main.version=$HULU_VERSION -X main.commit=$GIT_COMMIT" -o hulu
+
+mkdir -p output
+
+mv ./hulu output/
